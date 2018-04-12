3ade77f4fee7d13739f5978b07943ba7b98debce
Sebastian Limbach <bastilimbach@gmail.com>
2018-04-12 20:00:35 +0200

    Add always color to ls

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,4 +1,5 @@
 LSCOLORS="exfxcxdxbxegedabagacad"
+LS_COLORS="di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
 CLICOLOR=true
 JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_60.jdk/Contents/Home/fre"
 PLATIN_JAVA="/Library/Java/JavaVirtualMachines/jdk1.8.0_60.jdk/Contents/Home/fre"
