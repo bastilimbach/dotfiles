598d249ed102ae808ccb8a75223f22ff03652069
Sebastian Limbach <bastilimbach@gmail.com>
2018-10-12 20:56:56 +0200

    Fix ls commands

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,6 +1,6 @@
-alias ls="ls -F -A --color=always"
-alias ll="ls -lAh --color=always"
-alias l="ls -lh --color=always"
+alias ls="ls -F -A"
+alias ll="ls -lAh"
+alias l="ls -lh"
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
 alias o="open ."
 
