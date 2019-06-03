bd99bc31d69e6b19c63bdff2883e804ebaeda336
Sebastian Limbach <bastilimbach@gmail.com>
2019-06-03 13:18:49 +0200

    Remove option complete_aliases

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -32,9 +32,6 @@ setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share 
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
 
-# don't expand aliases _before_ completion has finished
-#   like: git comm-[tab]
-setopt complete_aliases
 PROMPT_GEOMETRY_GIT_CONFLICTS=true
 PROMPT_GEOMETRY_GIT_TIME=false
 GEOMETRY_SYMBOL_GIT_CONFLICTS_UNSOLVED="◇"
